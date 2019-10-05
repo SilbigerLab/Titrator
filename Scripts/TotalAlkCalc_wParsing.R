ec1bbbc7dc7c8d8e05ae8fad07a707a52cb3505f
Fields <jennifer.fields.855@my.csun.edu>
2019-10-05 11:41:28 -0700

    adding crm test from 10.5.2019 for backcalcuation

--- Scripts/TotalAlkCalc_wParsing.R
+++ Scripts/TotalAlkCalc_wParsing.R
@@ -46,17 +46,17 @@ library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
-path<-"Data/CRMTest_JF_10042019/" #the location of all your titration files
+path<-"Data/CRMTest_JF_10052019/" #the location of all your titration files
 
-massfile<-"MassCRMTest_10042019.csv" # name of your file with masses
+massfile<-"MassCRMTest_10052019.csv" # name of your file with masses
 
-titrationfile<-'Titrations-10_4_2019-Silbiger TA (EP)r1.csv'# name of the last titration file run
+titrationfile<-'Titrations-10_5_2019-Silbiger TA (EP)r1.csv'# name of the last titration file run
 
 
 
 # Date that the data were run
 
-date<-'10/4/2019'
+date<-'10/5/2019'
 
 
 
