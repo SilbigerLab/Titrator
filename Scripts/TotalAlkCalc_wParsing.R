2861f665ffaffaa19867550d4329aea7b70b3268
Kerlin <jamie.kerlin.481@my.csun.edu>
2022-04-08 19:22:41 -0700

    04082022 reefcrest TA samples March 2022

--- Scripts/TotalAlkCalc_wParsing.R
+++ Scripts/TotalAlkCalc_wParsing.R
@@ -48,21 +48,17 @@ library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
-path<-"Data/Moorea_March2022/04072022" #the location of all your titration files
+path<-"Data/Moorea_March2022/04082022" #the location of all your titration files
 
-massfile<-"SpringsMass_04072022.csv" # name of your file with masses
+massfile<-"Mass_04082022.csv" # name of your file with masses
 
-titrationfile<-'Titrations-4_7_2022-Silbiger TA (EP)r1.csv'# name of the last titration file run
+titrationfile<-'Titrations-4_8_2022-Silbiger TA (EP)r1.csv'# name of the last titration file run
 
 
 
 # Date that the data were run
 
-date<-'4/7/2022'
-
-
-
-
+date<-'4/8/2022'
 
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
