0e5ffb722aa084c32dd8254eaad67681548e9552
jennifer fields <jennifer.fields.855@my.csun.edu>
2022-05-16 15:32:48 -0700

    adding varari wells from Aug 2022

--- Scripts/TotalAlkCalc_wParsing.R
+++ Scripts/TotalAlkCalc_wParsing.R
@@ -48,17 +48,17 @@ library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
-path<-"Data/Moorea_March2022/04182022" #the location of all your titration files
+path<-"Data/VarariWellRerun_05162022" #the location of all your titration files
 
-massfile<-"Mass_04182022.csv" # name of your file with masses
+massfile<-"Mass_05162022.csv" # name of your file with masses
 
-titrationfile<-'Titrations-4_18_2022-Silbiger TA (EP)r1.csv'# name of the last titration file run
+titrationfile<-'Titrations-5_16_2022-Silbiger TA (EP)r1.csv'# name of the last titration file run
 
 
 
 # Date that the data were run
 
-date<-'4/18/2022'
+date<-'5/16/2022'
 
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
