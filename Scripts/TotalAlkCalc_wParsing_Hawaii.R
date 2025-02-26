2ec1155fe1b403512379beddb2512c90426f83da
Silbiger Computer UH <silbiger@hawaii.edu>
2025-02-26 13:54:42 -1000

    diamondhead 2025-02-23 haleytest

--- Scripts/TotalAlkCalc_wParsing_Hawaii.R
+++ Scripts/TotalAlkCalc_wParsing_Hawaii.R
@@ -44,13 +44,13 @@ library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
-path<-"Data/Chapuis/DiamondHead_20250223"
-massfile<-"mass20250224.csv" # name of your file with masses
-titrationfile<-"Titration-2_24_2025.csv" #<-name of last titration file run
+path<-"Data/Chapuis/DiamondHead_20250223_HaleyTest"
+massfile<-"Mass_20250226_CRM.csv" # name of your file with masses
+titrationfile<-"Titration-2_26_2025_CRM.csv" #<-name of last titration file run
 
 # Date that the data were run
 
-date<- '2/24/2025'
+date<- '2/26/2025'
 
 
 
