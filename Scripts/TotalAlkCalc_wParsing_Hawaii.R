155b88e384cdbe60d196b4fb0fb824a8fb4dc55e
Silbiger Computer UH <silbiger@hawaii.edu>
2025-06-25 10:41:03 -1000

    Ran CRM, practicing GitHub

--- Scripts/TotalAlkCalc_wParsing_Hawaii.R
+++ Scripts/TotalAlkCalc_wParsing_Hawaii.R
@@ -45,12 +45,12 @@ library(tidyverse)
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
 path<-"Data/Chapuis/Tidepools/Sandy_20250622"
-massfile<-"mass_20250624.csv" # name of your file with masses
-titrationfile<-"Titration-6_24_2025.csv" #<-name of last titration file run
+massfile<-"mass_20250625_CRM.csv" # name of your file with masses
+titrationfile<-"Titration-6_25_2025_CRM.csv" #<-name of last titration file run
 
 # Date that the data were run
 
-date<- '6/24/2025'
+date<- '6/25/2025'
 
 
 
