86d4570e01031b02ccca8d32f78fe391687741a0
Silbiger Computer UH <silbiger@hawaii.edu>
2026-08-31 17:23:12 -1000

    MP MARCH 18 1200 AND 2100 DATA ADD

--- Scripts/TotalAlkCalc_wParsing_Hawaii.R
+++ Scripts/TotalAlkCalc_wParsing_Hawaii.R
@@ -44,11 +44,11 @@ library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
-path<-"Data/Chapuis/Tiles/Pilot_PI_20260826"
-massfile<-"mass_20260827.csv" # name of your file with masses
-titrationfile<-"Titration-8_27_2026.csv" # name of your titration file run
+path<-"Data/Powell/BioFac_18March2026_1200_083126"
+massfile<-"mass_083126.csv" # name of your file with masses
+titrationfile<-"Titration-8_31_2026.csv" # name of your titration file run
 
-date<- '8/27/2026' # Date that the data were run
+date<- '8/31/2026' # Date that the data were run
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
 
